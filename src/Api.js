8bb869eb44188a6dc8f7f1ed40c801958cbc965e
Karel Kryštof Dostál <karel.krystof.dostal@beanstudent.cz>
2024-05-15 10:29:51 +0200

    final verze

--- src/Api.js
+++ src/Api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
+import { Card, CardContent, Box } from "@mui/material";
 
 
 function Api() {
@@ -33,7 +34,7 @@ function Api() {
       
       
       <Typography variant="h3">Filmy</Typography>
-      <Grid container spacing={2}>
+      {/*<Grid container spacing={2}>
         {filmy.map(film => (
           <>
             <Grid item xs={20}>
@@ -43,11 +44,44 @@ function Api() {
                 <Typography variant="subtitle2">{film.title}</Typography>
                 </div>
               </Paper>
+              
             </Grid>
           </>
         ))
         }
-      </Grid >
+      </Grid >*/}
+      <Grid container spacing={2}>
+              {filmy.map(film => (
+                <>
+                <Grid item xs={4}>
+                <Card sx={{ maxWidth: 200 }} onClick={() => nastavVybranyFilm(film.title)}>
+                  <Box  sx =  {{flexDirection: 'column'}}>
+                  <CardContent>
+                  <img src={film.image} style={{ width: "150px",height: "150px"}}></img>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {film.title}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                      režisér: {film.creator} 
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary" component="div">
+                      žánr: {film.genre}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary" component="div">
+                      rok: {film.year}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary" component="div">
+                      počet epizod: {film.episodes}
+                      </Typography>
+                  </CardContent>
+                  </Box>
+                  
+                </Card>
+                </Grid>
+              </>
+              ))
+             }
+             </Grid>
 
 
     </>
